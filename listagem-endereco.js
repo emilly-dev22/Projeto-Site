805d07fbdc69baeb800fe89c9af1a2b6ebebab94
emilly <emillysilvaduardo@gmail.com>
2025-05-21 19:20:54 -0300

    inetgrando objeto a html

--- listagem-endereco.js
+++ listagem-endereco.js
@@ -1,22 +1,45 @@
 async function listagemEdereco() {
     let token = JSON.parse(localStorage.getItem("user"));
-    
+
     let api = await fetch("https://go-wash-api.onrender.com/api/auth/address", {
-        method:"GET",
-        headers: { 
+        method: "GET",
+        headers: {
             "Content-Type": "application/json",
-            "Authorization": "Bearer "+token.access_token
-         }
+            "Authorization": "Bearer " + token.access_token
+        }
+
+
     })
 
     if (api.ok) {
         let response = await api.json()
-        console.log(response); // Fazer com que o endereco retornado pela API apareça na tabela do HTML
+        let tbody = document.getElementById("listagem");
+
+        tbody.innerHTML = "";
+
+        response.data.forEach(element => {
+            let el = `
+            <tr>
+                <td>${element.title}</td>
+                <td>${element.cep}</td>
+                <td>${element.address}</td>
+                <td>${element.number}</td>
+                <td>
+                    <a href="#"><button>Atualizar</button></a>
+                    <a href="#"><button>Deletar</button></a> 
+                </td>
+            </tr>
+        `;
+            tbody.innerHTML += el;
+        });
+
     } else {
         let responseError = await api.json()
         console.log(responseError)
     }
-    
+
+
+
 }
 
 listagemEdereco();
